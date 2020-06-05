1590ceec6dac4e5f810f326833b07da9ea1922c8
Tanya Sinha <sinha.tanyacse@gmail.com>
2020-06-06 01:25:33 +0530

    completed- graphQL

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyBtNR6BrWtpi-ieYsKcqxFIg516GfOCBxw",
+  authDomain: "online-shopping-react-db.firebaseapp.com",
+  databaseURL: "https://online-shopping-react-db.firebaseio.com",
+  projectId: "online-shopping-react-db",
+  storageBucket: "online-shopping-react-db.appspot.com",
+  messagingSenderId: "173181341949",
+  appId: "1:173181341949:web:ad4e80afbe3abe8646ae94",
+  measurementId: "G-3T828XD5BC"
 };
 
 firebase.initializeApp(config);
--- src/index.js
+++ src/index.js
@@ -4,18 +4,43 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import {ApolloProvider} from 'react-apollo';
+import {createHttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {ApolloClient} from  'apollo-boost';
+
 import { store, persistor } from './redux/store';
 
 import './index.css';
-import App from './App';
+import {default as App} from './App/App.container';
+
+import {resolvers, typeDefs} from './graphql/resolvers';
+import { default as data } from './graphql/initial-data';
+
+const httpLink = createHttpLink({
+  uri: 'https://crwn-clothing.com/'
+})
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+  resolvers,
+  typeDefs
+});
+
+client.writeData({ data });
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
